52b7598e4e7999c4945a2c59abc6ad2d9981b023
Sindhu-Ashok <sin4274ashok@gmail.com>
2022-06-01 12:23:16 +0530

    NewProject

---
+++ src/test/java/DemoTest/DemoSampleTest.java
@@ -0,0 +1,14 @@
+package DemoTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoSampleTest {
+	
+	@Test
+	public void testName() throws Exception 
+	{
+		Reporter.log("GITBASH",true);
+	}
+
+}
